ead29703dcee0f404704246f27e8fb6f7c2bcd69
Akerele, Tosin James <akerele.james1699@gmail.com>
2023-06-17 08:52:09 +0100

    saving progress so far

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = () => setIsClicked(!isClicked);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{
+          backgroundColor: isClicked ? "blue" : "red",
+          color: "#ffffff",
+        }}
+        onClick={handleClick}
+      >
+        {isClicked ? "Change to red" : "Change to blue"}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// testing 3 things.
+// 1. button should have correct initial color
+// 2. button should have correct initial Text.
+// 3. Button should turn blue when clikced.
+
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // let's test for test2.
+  expect(colorButton).toHaveTextContent("Change to blue");
+
+  // let's test for test3.
+  fireEvent.click(colorButton);
+
+  // let's test for test3.
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("button enabled and checkbox state", () => {
+  render(<App />);
+  const buttonEnabled = screen.getByRole("button", { name: "Change to blue" });
+  expect(buttonEnabled).toBeEnabled();
+
+  const checkboxInput = screen.getByRole("checkbox");
+  expect(checkboxInput).not.toBeChecked();
 });
